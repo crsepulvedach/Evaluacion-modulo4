493c153aa125bad06f842b6f406c74ea81f4c4c9
Cristian Sepulveda <crsepulvedach@gmail.com>
2025-06-30 18:13:11 -0400

    Test Selenium 02

--- src/test/java/com/usuario/UsuarioTest.java
+++ src/test/java/com/usuario/UsuarioTest.java
@@ -1,6 +1,6 @@
 package com.usuario;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -12,5 +12,11 @@ public class UsuarioTest {
         Usuario usuario = new Usuario("Juan", 70.0);
         usuario.actualizarPeso(75.0); // Se debería modificar la lógica.
         assertEquals(75.0, usuario.getPeso(), "El peso no se actualiza correctamente");
+    }
+
+    @Test
+    public void testMostrarInformacion() {
+        Usuario usuario = new Usuario("Ana", 60.0);
+        usuario.mostrarInformacion(); // Puede ser que solo imprima a consola, se puede usar para comprobar manualmente.
     }
 }
